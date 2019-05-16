f285970e6b539bc88fe6324c19b7818afc92287b
GilangRamadan <40588613+gilangramadn@users.noreply.github.com>
2019-05-16 23:32:59 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,8 +2,11 @@
 <html>
 <head>
 	<title>Input Data</title>
-  </style>
-
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 		<CENTER>
@@ -85,8 +88,8 @@
 			
 		</table><br><center>
 		<button class="btn btn-success">Tambah</button>
-		<button  class="btn btn-primary" type="reset">Reset</button>
-		</center><br>
+		<button  class="btn btn-success" type="reset">Reset</button>
+		</center>
 		<br>
 		</form>
 	</div>
