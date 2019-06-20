54b9fbd1f8c79e215c318a9fdfc45a8e51bb1aea
GilangRamadan <40588613+gilangramadn@users.noreply.github.com>
2019-06-20 22:13:50 +0700

    Update produkpc.php

--- produkpc.php
+++ produkpc.php
@@ -6,6 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
   <script src="jquery.min.js"></script>
   <script src="bootstrap.min.js"></script>
 
@@ -32,8 +33,46 @@
 </div>
 <body>
 <div class="container">
-	<div class="jumbotron" style="margin: 0px;background: #8abce6;padding-top: 0px;">
+	<div class="jumbotron" style="margin: 0px;background-color: rgba(245, 245, 245, 0.4);padding-top: 0px;">
   <hr style="border: 0px solid #fff">
+  <?php 
+		if(isset($_GET['pesan'])){
+			$pesan = $_GET['pesan'];
+    if($pesan == 'berhasil'){
+    	?>
+    	<div class="alert alert-success" role="alert">
+		  <b>Data Berhasil di Masukkan</b>
+		</div><?php 
+		    }
+		    else if($pesan == 'update'){
+		    	?>
+		    	<div class="alert alert-success" role="alert">
+		  <b>Data Berhasil di Update</b>
+		</div>
+		    	<?php
+		    }
+		    else if($pesan == 'hapus'){
+		    	?>
+		    	<div class="alert alert-danger" role="alert">
+		  <b>Data Berhasil di hapus</b>
+		</div>
+		    	<?php
+		    }else if($pesan == 'gagal'){
+		    	?>
+		    	<div class="alert alert-danger" role="alert">
+		  <b>Produk sedang digunakan, tidak bisa mengubah nama</b>
+		</div>
+		    	<?php
+		    }else if($pesan == 'gagalhapus'){
+		    	?>
+		    	<div class="alert alert-danger" role="alert">
+		  <b>Produk sedang digunakan, tidak bisa dihapus</b>
+		</div>
+		    	<?php
+		    }
+
+		}
+		    ?>
   <h1 style="font-family: fantasy;
     text-transform: uppercase;
     -webkit-text-stroke: 2px #ffffff;">Data Produk PC</h1>
@@ -45,10 +84,9 @@
 		<center>
 	<table id="mytable" class="table table-bordred table-striped">
 		 <thead>
-			<th >Nama Produk PC</th>
-			<th >Jenis Produk PC</th>
-			
-				<!--		<th >Tools</th>-->
+			<th style="color: white;">Nama Produk PC</th>
+			<th style="color: white;">Jenis Produk PC</th>
+			<th style="color: white;">Tools</th>
 			</tr>
 	
 		</thead>
@@ -62,18 +100,10 @@
 		<tr > 
 			<td class="info"><?php echo $row['nama_produk'];?></td><!--namaatribut-->
 			<td class="warning"><?php echo $row['jenis_produk'];?></td>
-			<!--
-			<a href="update.php?id=<?php echo $row['nama_produk']; ?>" >Update</a>
-
-			<a><a href="delete.php?id=<?php echo $row['nama_produk']; ?> ">Delete</a>
-
-			<a><a href="insert.php?id=<?php echo $row['nama_produk']; ?>">Insert</a>-->
-</a></td>
-			
-				
-			
-				
-		
+			<td>
+			<a href="updateproduk.php?id=<?php echo $row['nama_produk']; ?>" >Update</a> ||
+			<a href="deleteproduk.php?id=<?php echo $row['nama_produk']; ?> ">Delete</a>
+			</td>
 		</tr>
 			 </tbody>
 		<?php
@@ -84,6 +114,6 @@
 </div>
 </center>
 	</table>
-
+<center><a href="inputproduk.php"><button class="btn btn-danger">Tambah Produk PC</button></a></center>
 </body>
 </html>
