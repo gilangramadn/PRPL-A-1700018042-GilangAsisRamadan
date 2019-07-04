f1c42db6a09ef2981fda68fe992785761a6eb380
GilangRamadan <40588613+gilangramadn@users.noreply.github.com>
2019-07-04 13:56:58 +0000

    Delete deletekaryawan.php

--- deletekaryawan.php
+++
@@ -1,9 +0,0 @@
-<?php
- include '../koneksi.php';
-
-$id_karyawan = $_GET['id'];
-
-mysqli_query($koneksi,"DELETE FROM karyawan where id_karyawan = '$id_karyawan'");
-// die("testing");
-header("location:./../karyawan.php?pesan=hapus");
- ?>
